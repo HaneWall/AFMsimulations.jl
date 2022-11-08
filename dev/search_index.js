bd990a72e03a57d7c1703c7b716da9f42fc27bb8
Documenter.jl <documenter@juliadocs.github.io>
2022-11-08 12:05:50 +0000

    build based on 58a5756

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = AFMsimulations","category":"page"},{"location":"#AFMsimulations","page":"Home","title":"AFMsimulations","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for AFMsimulations.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [AFMsimulations]","category":"page"}]
+}
