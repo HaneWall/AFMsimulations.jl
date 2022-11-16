25c247fb3b3218353ae5d15763d591470fb1cd27
HaneWall <48854946+HaneWall@users.noreply.github.com>
2022-11-16 17:18:35 +0100

    TODO: write my own Gabor Transform

--- examples/analyzing_AFM_data.jl
+++ examples/analyzing_AFM_data.jl
@@ -20,12 +20,24 @@ Todos:
 Δf = 1/Δt
 
 
+
+# fs = 44_100  # Hz
+# ts = range(0, stop=5, step=1/fs)  # seconds
+# signal = @. sin(2π*1_000*ts^2)   # sweep over f = (1000*ts) Hz
+# #  plot(ts, signal)   # > 200K points, better to use InspectDR
+# n = length(signal)
+# nw = n÷50
+# spec = spectrogram(signal, nw, nw÷2; fs=fs)
+
+
 N = 1_000_000
-number_of_peices = 10_000
+number_of_peices = 10
 samples_per_fft = N÷number_of_peices
-spec = spectrogram(data[1:N, :mV], N, samples_per_fft; fs=Δf)
+spec = spectrogram(data[1:N, :mV], 300; fs=Δf)
+
+
 
 fig = Figure(resolution = (500, 500))
 ax = Axis(fig[1, 1])
-heatmap!(ax, collect(0:1:N).*Δt, spec.freq, spec.power)
+heatmap!(ax, pow2db.(spec.power))
 fig
