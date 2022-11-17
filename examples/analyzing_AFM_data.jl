2440cbb4d7fc14ce789039340a2b61f279847781
HaneWall <48854946+HaneWall@users.noreply.github.com>
2022-11-17 13:09:26 +0100

    Chirp visualized

--- examples/analyzing_AFM_data.jl
+++ examples/analyzing_AFM_data.jl
@@ -8,7 +8,6 @@ data = DataFrame(CSV.File("./TimeSeries_3_14_11.csv", header=true))
 """
 BIG PROBLEM: How to determine the end of one frequency. 
 Todos:
-    - Implement Gabor Transform to determine endpoints of one constant frequency 
     - create batches for each individual frequency --> SINDy determines coefficients for one model
 """
 
@@ -19,13 +18,13 @@ Todos:
 Δt = (data[2, :ms] - data[1, :ms])*1e-3 
 Δf = 1/Δt
 
+CairoMakie.activate!()
 #N = 70_000_000
-spec = spectrogram(data[1:end, :mV], 50_000, 1_000; fs=Δf)
+spec = spectrogram(data[1:end, :mV], 100_000, 4_000; fs=Δf, window=blackman)
 
-
-
-fig = Figure(resolution = (500, 500))
-ax = Axis(fig[1, 1], xlabel="f[Hz]")
-heatmap!(ax, spec.freq, spec.time, pow2db.(spec.power))
-xlims!(ax, [5e4, 6e4])
+fig = Figure(resolution = (800, 1200), fontsize=24)
+ax = Axis(fig[1, 1], xlabel=L"f[Hz]", ylabel=L"t[s]")
+hmap = heatmap!(ax, spec.freq, spec.time, pow2db.(spec.power), colormap = :turbo, colorrange=(-25, 20))
+Colorbar(fig[1, 2], hmap; label = "Power in dB", width = 15, ticksize = 15, tickalign = 1)
+xlims!(ax, [4.8e4, 6.1e4])
 fig
