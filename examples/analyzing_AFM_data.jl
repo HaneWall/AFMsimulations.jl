a7701e00dae5bc81b20ace7cb1ed3a160a26a765
HaneWall <48854946+HaneWall@users.noreply.github.com>
2022-11-17 12:29:50 +0100

    Gabor transform --> Sweep can be detected.  Use of windows?

--- examples/analyzing_AFM_data.jl
+++ examples/analyzing_AFM_data.jl
@@ -19,25 +19,13 @@ Todos:
 Δt = (data[2, :ms] - data[1, :ms])*1e-3 
 Δf = 1/Δt
 
-
-
-# fs = 44_100  # Hz
-# ts = range(0, stop=5, step=1/fs)  # seconds
-# signal = @. sin(2π*1_000*ts^2)   # sweep over f = (1000*ts) Hz
-# #  plot(ts, signal)   # > 200K points, better to use InspectDR
-# n = length(signal)
-# nw = n÷50
-# spec = spectrogram(signal, nw, nw÷2; fs=fs)
-
-
-N = 1_000_000
-number_of_peices = 10
-samples_per_fft = N÷number_of_peices
-spec = spectrogram(data[1:N, :mV], 300; fs=Δf)
+#N = 70_000_000
+spec = spectrogram(data[1:end, :mV], 50_000, 1_000; fs=Δf)
 
 
 
 fig = Figure(resolution = (500, 500))
-ax = Axis(fig[1, 1])
-heatmap!(ax, pow2db.(spec.power))
+ax = Axis(fig[1, 1], xlabel="f[Hz]")
+heatmap!(ax, spec.freq, spec.time, pow2db.(spec.power))
+xlims!(ax, [5e4, 6e4])
 fig
