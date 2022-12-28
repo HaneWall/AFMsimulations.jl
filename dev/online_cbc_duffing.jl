4a51986b470ce966f12eb7ec5443566e5a39c781
HaneWall <48854946+HaneWall@users.noreply.github.com>
2022-12-28 15:44:44 +0100

    push_duffing_complete

--- dev/online_cbc_duffing.jl
+++ dev/online_cbc_duffing.jl
@@ -5,6 +5,34 @@ using BenchmarkTools, LinearAlgebra, Statistics, CairoMakie, ProgressBars
 This code should be able to reconstruct some results from alrdy 
 published cbc-experiments. 
 """
+@with_kw mutable struct p_duffing_no_control_ampl_sweep
+    k_1 :: Float64 = 1.
+    k_2 :: Float64 = 1. 
+    k_3 :: Float64 = 1.
+    N :: Int64 = 80
+    i :: Int64 = 0
+    ϕ :: Float64 = 0. 
+    Γ_beg :: Float64 = 10. 
+    Γ_end :: Float64 = 31. 
+    h :: Float64 = (Γ_end - Γ_beg)/N
+    Γ :: Float64 = Γ_beg
+    Ω :: Float64 = 4. 
+end
+
+
+@with_kw mutable struct p_duffing_no_control
+    k_1 :: Float64 = 1.
+    k_2 :: Float64 = 1. 
+    k_3 :: Float64 = 1.
+    Ω_beg :: Float64 = 1.5 
+    Ω_end :: Float64 = 4.
+    N :: Int64 = 80
+    i :: Int64 = 0
+    ϕ :: Float64 = 0. 
+    Γ :: Float64 = 12.
+    Ω_arr :: Vector{Float64} = collect(range(Ω_beg, Ω_end, length=N))
+    Ω :: Float64 = Ω_beg
+end
 
 @with_kw mutable struct p_abel_1
     k_1 ::Float64 = 1.
@@ -99,6 +127,14 @@ end
 Functions that define ourt problem and the cbc-algorithm. 
 """
 
+function f_duffing_no_control(u, p, t)
+    @unpack k_1, k_2, k_3, Γ, Ω, ϕ = p
+    dx = u[2]
+    dy = - k_1 * u[1] - k_2 * u[2] - k_3 * u[1]^3 + Γ * sin(Ω*t + ϕ)
+    return SA[dx, dy]
+end
+
+
 function f_duffing(u, p, t)
     @unpack k_1, k_2, k_3, control = p
     dx = u[2]
@@ -106,10 +142,108 @@ function f_duffing(u, p, t)
     return SA[dx, dy]
 end
 
+
+function freq_sweep_stat(p::p_duffing_no_control)
+    Δt = 0.005
+    T_sim = 3000
+    tspan = (0, T_sim)
+    u_0 = SA[0.; 0.]
+    ampl_container = zeros(Float64, p.N)
+    phi_container = zeros(Float64, p.N)
+    @inbounds for idx in ProgressBar(1:p.N)
+        p.Ω = p.Ω_arr[idx]
+        T_period = 2π/(Δt * p.Ω) 
+        prob = ODEProblem(f_duffing_no_control, u_0, tspan, p)
+        integrator = init(prob, AutoTsit5(Rosenbrock23()), dt=Δt, adaptive=false) 
+        solve!(integrator)
+        # amplitude detection algorithm (fft not necessary)
+        T_period_int = ceil(Int, T_period)
+        base = sinusoidal_bases(4, p.Ω, integrator.t - T_period * Δt, integrator.t)
+        t_b = collect(LinRange(integrator.t - T_period*Δt, integrator.t, T_period_int))
+        θ = lsq_regression(t_b, integrator.sol[1, end-(T_period_int-1):end], base)
+        ampl_container[idx] = sqrt(θ[2]^2 + θ[3]^2)
+        φ = atan(θ[3], θ[2]) 
+        # phase difference input and output 
+        phi_container[idx] = (p.ϕ - φ + π)%2π - π
+        # phase conservation 
+        p.ϕ = (p.ϕ + T_sim*p.Ω)%2π
+        # new u_0 is last entry of previous simulation 
+        u_0 = SA[integrator.sol[1, end]; integrator.sol[2, end]]
+    end
+    return ampl_container, phi_container
+end
+
+### amplitude sweeps, first no control, just up and down 
+function  ampl_sweep(p::p_duffing_no_control_ampl_sweep)
+    u0 =SA[0. ; 0.]
+    Δt = 0.005
+    tspan = (0., 25_000.)
+    t_period = 1/p.Ω * 2π/Δt
+    response_amplitude = Float64[]
+    forcing_amplitude = Float64[]
+   
+    std_buffer = zeros(Float64, 30)
+    function affect!(integrator)
+        # this affect function takes place once every period (like a Poincare/stroboscopic map)
+        # we count the periods i with the given parameters 
+        integrator.p.i += 1
+        # we get the amplitude and phase of the response from on the fly fourier coefficients
+        A = sqrt(w[2]^2 + w[3]^2)
+        # In order to detect the end of a transient behavior we create a 30
+        # period buffer of the amplitude. If the standard deviation of this
+        # buffer reaches a tolerance we converged. We can make this idea
+        # dimensionless by division with the mean. 
+        popfirst!(std_buffer)
+        push!(std_buffer, A)
+        std_mean = std(std_buffer)/mean(std_buffer)  
+        if log.(10, std_mean) < -5.5 && integrator.p.i > 30
+            # If we converge for a given input force, we increase the the input
+            # force. 
+            push!(response_amplitude, A)
+            push!(forcing_amplitude, integrator.p.Γ)
+            #restart period counter
+            integrator.p.i = 0
+            #continuation in excitation
+            integrator.p.Γ += integrator.p.h
+        end
+        nothing
+    end 
+
+    periodic_cb = PeriodicCallback(affect!, t_period*Δt, initial_affect=false) # Poincare/Stroboscopic Map 
+    prob = ODEProblem(f_duffing_no_control, u0, tspan, p) 
+    integrator = init(prob, callback=periodic_cb, alg=AutoTsit5(Rosenbrock23()), dt = Δt, adaptive=false, save_everystep=false)
+
+    k = 5                                           # approx signal up to 5th harmonic
+    base = create_bases(k, integrator.p.Ω)          # basis for x online fourier
+    
+    # preallocate basis vectors and fourier coefficients
+    w = zeros(2*k + 1)
+    b = zeros(2*k + 1)
+    steps = floor(Int64, tspan[2]/Δt)
+    μ = Δt
+    for st in ProgressBar(1:steps)
+        update_basis!(b, base, integrator.t)
+        update_weights!(w, b, integrator.u[1], μ)
+        step!(integrator)
+        if integrator.p.h > 0
+            if integrator.p.Γ >= integrator.p.Γ_end
+                terminate!(integrator)
+                break
+            end
+        else
+            if integrator.p.Γ <= integrator.p.Γ_end
+                terminate!(integrator)
+                break
+            end 
+        end
+    end
+    return response_amplitude, forcing_amplitude 
+end
+
 function cbc_sweep(p::p_abel_1)
     u0 = SA[0. ; 0.]
     Δt = 0.005
-    tspan = (0., 19_000.)
+    tspan = (0., 25_000.)
     t_period = 1/p.Ω * 2π/Δt
     response_amplitude = Float64[]
     response_phase = Float64[]
@@ -118,38 +252,50 @@ function cbc_sweep(p::p_abel_1)
     t_buffer = zeros(Float64, ceil(Int64, t_period)) 
     Γ_buffer = zeros(Float64, ceil(Int64, t_period))
     std_buffer = zeros(Float64, 30)
-    
+
     function affect!(integrator)
+        # this affect function takes place every period (like a Poincare map)
+        # we count the periods i with the given parameters 
         integrator.p.i += 1
+        # we get the amplitude and phase of the response from on the fly fourier coefficients
         A = sqrt(w[2]^2 + w[3]^2)
         φ = atan(w[3], w[2])
-        ϕ = (integrator.t * integrator.p.Ω)%2π
-        #Γ = sqrt((integrator.p.k_p * (integrator.p.X_1s_target - w[2]*b[2]) - integrator.p.Ω * integrator.p.k_d*(integrator.p.X_1c_target - w[3]*b[3]))^2 + 
-        #         (integrator.p.k_p * (integrator.p.X_1c_target - w[3]*b[3]) + integrator.p.Ω * integrator.p.k_d*(integrator.p.X_1s_target - w[2]*b[2]))^2)
+        # We get the amplitude and phase of the effective forcing / control 
         ξ = lsq_regression(t_buffer, Γ_buffer, sin_base)
         Γ = sqrt(ξ[2]^2 + ξ[3]^2)
+        ζ = (integrator.t * integrator.p.Ω)
+        Γ_phi = (ζ - atan(ξ[3], ξ[2]) + π)%2π - π
+        # In order to detect the end of a transient behavior we create a 30
+        # period buffer of the amplitude. If the standard deviation of this
+        # buffer reaches a tolerance we converged. We can make this idea
+        # dimensionless by division with the mean. 
         popfirst!(std_buffer)
         push!(std_buffer, A)
         std_mean = std(std_buffer)/mean(std_buffer)  
-        if log.(10, std_mean) < -4.5 && integrator.p.i > 30
-            # if we converge for a given input force, we increase the the input force
-            push!(response_phase, (ϕ - φ + π)%2π - π)
+        if log.(10, std_mean) < -5.5 && integrator.p.i > 30
+            # If we converge for a given input force, we increase the the input
+            # force. 
+            push!(response_phase, φ)
             push!(response_amplitude, A)
             push!(eff_forcing_amplitude, Γ)
+            #restart period counter
             integrator.p.i = 0
+            #continuation in response
             integrator.p.X_1s_target += integrator.p.h
         end
         nothing
     end
 
-    periodic_cb = PeriodicCallback(affect!, t_period*Δt, initial_affect=false)
+    periodic_cb = PeriodicCallback(affect!, t_period*Δt, initial_affect=false) # Poincare/Stroboscopic Map 
     prob = ODEProblem(f_duffing, u0, tspan, p) 
     integrator = init(prob, callback=periodic_cb, alg=AutoTsit5(Rosenbrock23()), dt = Δt, adaptive=false, save_everystep=false)
 
-    k = 5
-    base = create_bases(k, integrator.p.Ω)
-    base_dot = create_dot_bases(k, integrator.p.Ω)
-    sin_base = sinusoidal_bases(k, integrator.p.Ω, 0, 2π)
+    k = 5                                           # approx signal up to 5th harmonic
+    base = create_bases(k, integrator.p.Ω)          # basis for x online fourier
+    base_dot = create_dot_bases(k, integrator.p.Ω)  # basis for x_dot online fourier
+    sin_base = sinusoidal_bases(k, integrator.p.Ω, 0, 2π) # fourier base - offline Least-Squares
+    
+    # preallocate basis vectors and fourier coefficients
     ξ = zeros(2*k + 1)
     w = zeros(2*k + 1)
     b = zeros(2*k + 1)
@@ -177,20 +323,50 @@ function cbc_sweep(p::p_abel_1)
         popfirst!(Γ_buffer)
         push!(t_buffer, integrator.t)
         push!(Γ_buffer, integrator.p.control)
-        #push!(p.control_buff, integrator.p.control)
         step!(integrator)
         if length(response_amplitude)>1 && eff_forcing_amplitude[end] >= 31.
             terminate!(integrator)
             break
         end
     end
-    return response_amplitude, response_phase, eff_forcing_amplitude, integrator.sol
+    return response_amplitude, response_phase, eff_forcing_amplitude #, integrator.sol
 end
 
-omegas = [2. + i*0.25 for i in 1:1:8]
-paras = [p_abel_1(X_1s_target=1., h=0.05, k_d=30.1, k_p=22.6, Ω=omegas[i]) for i in 1:8] 
+## some amplitude sweeps without control 
 
-#R_1, φ_R_1, Γ_1, sol_1 = cbc_sweep(paras[1])
+p_ampl_4_fwd = p_duffing_no_control_ampl_sweep(Γ_beg = 10. , Γ_end = 31.)
+p_ampl_4_bwd = p_duffing_no_control_ampl_sweep(Γ_beg = 31., Γ_end = 10.)
+
+a_fwd, Γ_fwd = ampl_sweep(p_ampl_4_fwd)
+a_bwd, Γ_bwd = ampl_sweep(p_ampl_4_bwd) 
+
+
+## some frequency sweeps to compare theory to real sweeps
+p_no_fwd = p_duffing_no_control()
+p_no_bwd = p_duffing_no_control(Ω_beg = 4., Ω_end=1.5)
+
+p_no_fwd_9 = p_duffing_no_control(Γ = 9.)
+p_no_bwd_9 = p_duffing_no_control(Ω_beg = 4., Ω_end=1.5, Γ = 9.)
+
+p_no_fwd_45 = p_duffing_no_control(Γ = 4.5)
+p_no_bwd_45 = p_duffing_no_control(Ω_beg = 4., Ω_end=1.5, Γ = 4.5)
+
+r_fwd, phi_fwd = freq_sweep_stat(p_no_fwd)
+r_bwd, phi_bwd = freq_sweep_stat(p_no_bwd)
+
+r_fwd_9, phi_fwd_9 = freq_sweep_stat(p_no_fwd_9)
+r_bwd_9, phi_bwd_9 = freq_sweep_stat(p_no_bwd_9)
+
+r_fwd_45, phi_fwd_45 = freq_sweep_stat(p_no_fwd_45)
+r_bwd_45, phi_bwd_45 = freq_sweep_stat(p_no_bwd_45)
+
+
+## amplitude sweeps with control 
+
+omegas = [1.5 + i*0.25 for i in 1:1:10]
+paras = [p_abel_1(X_1s_target=0.1, h=0.05, k_d=2.1, k_p=0., Ω=omegas[i]) for i in 1:10] 
+
+# TODO: change names, fked up with the numbers
 R_2, φ_R_2, Γ_2 = cbc_sweep(paras[1])
 R_3, φ_R_3, Γ_3 = cbc_sweep(paras[2])
 R_4, φ_R_4, Γ_4 = cbc_sweep(paras[3])
@@ -199,65 +375,154 @@ R_6, φ_R_6, Γ_6 = cbc_sweep(paras[5])
 R_7, φ_R_7, Γ_7 = cbc_sweep(paras[6])
 R_8, φ_R_8, Γ_8 = cbc_sweep(paras[7])
 R_9, φ_R_9, Γ_9 = cbc_sweep(paras[8])
+R_10, φ_R_10, Γ_10 = cbc_sweep(paras[9])
+R_11, φ_R_11, Γ_11 = cbc_sweep(paras[10])
 
+## let us first compare a amplitude sweep without control and with control 
+CairoMakie.activate!(type="svg")
+fig = Figure(resolution = (800, 800), fontsize=34, fonts = (; regular = "CMU Serif"))
+ax = Axis(fig[1,1], xlabel=L"\Gamma", ylabel=L"R")
+scatter!(ax, Γ_fwd, a_fwd, marker=:rect, markersize=24, strokecolor=:black, strokewidth=2, color=(:black, 0.), label="Forward")
+scatter!(ax, Γ_bwd, a_bwd, marker=:circle, markersize=24, strokecolor=:red, strokewidth=2, color=(:black, 0.), label="Backward")
+scatterlines!(ax, Γ_11, R_11, label="CBC")
+axislegend(L"\omega = 4", position=:lt)
+xlims!(ax, (10., 31.))
+fig
+
+
+#using GLMakie
 CairoMakie.activate!(type="svg")
 fig = Figure(resolution=(800, 800))
-ax1 = Axis3(fig[1,1], azimuth=7.3*π/4, perspectiveness=0.)
+ax1 = Axis3(fig[1,1], azimuth=7*π/4, perspectiveness=0.)
 #scatterlines!(ax1, Γ_1, R_1)
-scatterlines!(ax1, 2.25 .* ones(length(R_2)), Γ_2, R_2)
-scatterlines!(ax1, 2.5 .* ones(length(R_3)), Γ_3, R_3)
-scatterlines!(ax1, 2.75 .* ones(length(R_4)), Γ_4, R_4)
-scatterlines!(ax1, 3. .* ones(length(R_5)), Γ_5, R_5)
-scatterlines!(ax1, 3.25 .* ones(length(R_6)), Γ_6, R_6)
-scatterlines!(ax1, 3.5 .* ones(length(R_7)), Γ_7, R_7)
-scatterlines!(ax1, 3.75 .* ones(length(R_8)), Γ_8, R_8)
-scatterlines!(ax1, 4. .* ones(length(R_9)), Γ_9, R_9)
+scatterlines!(ax1, 1.75 .* ones(length(R_2)), Γ_2, R_2)
+scatterlines!(ax1, 2. .* ones(length(R_3)), Γ_3, R_3)
+scatterlines!(ax1, 2.25 .* ones(length(R_4)), Γ_4, R_4)
+scatterlines!(ax1, 2.5 .* ones(length(R_5)), Γ_5, R_5)
+scatterlines!(ax1, 2.75 .* ones(length(R_6)), Γ_6, R_6)
+scatterlines!(ax1, 3. .* ones(length(R_7)), Γ_7, R_7)
+scatterlines!(ax1, 3.25 .* ones(length(R_8)), Γ_8, R_8)
+scatterlines!(ax1, 3.5 .* ones(length(R_9)), Γ_9, R_9)
+scatterlines!(ax1, 3.75 .* ones(length(R_10)), Γ_10, R_10)
+scatterlines!(ax1, 4. .* ones(length(R_11)), Γ_11, R_11)
 fig
 
 
-# using GaussianProcesses
-# using Plots
-# ws = [2.5 .* ones(length(R_2)); 3. .* ones(length(R_3)); 3.5 .* ones(length(R_4)); 4. .* ones(length(R_5))]  
-# Gs = [Γ_2; Γ_3; Γ_4; Γ_5]
-# Rs = [R_2; R_3; R_4; R_5]
-# Xs = transpose(hcat(Gs, ws))
-# gp2 = GP(Xs,Rs, MeanZero(), SE(0., 0.))
-# Plots.surface(gp2)
+using GaussianProcesses
+ws = [1.75 .* ones(length(R_2)); 2. * ones(length(R_3)); 2.25 .* ones(length(R_4)); 2.5 .* ones(length(R_5)); 2.75 .* ones(length(R_6)); 3. .* ones(length(R_7)); 3.25 .* ones(length(R_8)); 3.5 .* ones(length(R_9)); 3.75 .* ones(length(R_10)); 4. .* ones(length(R_11)) ]  
+Gs = [Γ_2; Γ_3; Γ_4; Γ_5; Γ_6; Γ_7; Γ_8; Γ_9; Γ_10; Γ_11]
+Rs = [R_2; R_3; R_4; R_5; R_6; R_7; R_8; R_9; R_10; R_11]
+φs = [φ_R_2; φ_R_3; φ_R_4; φ_R_5; φ_R_6; φ_R_7; φ_R_8; φ_R_9; φ_R_10; φ_R_11] 
+Xs = transpose(hcat(Rs, ws))
+Xs_phi = transpose(hcat(φs, ws))
+gp2 = GP(Xs,Gs, MeanZero(), SE(0., 0.))
+gpphi = GP(Xs_phi, Gs, MeanZero(), SE(0., 0.)) 
 
-## testing dot base 
+xmin, xmax =  (minimum(gp2.x[1,:]), maximum(gp2.x[1,:]))
+ymin, ymax = (minimum(gp2.x[2,:]), maximum(gp2.x[2,:]))
+x = range(xmin, stop=xmax, length=120)
+y = range(ymin, stop=ymax, length=120)
+xgrid = repeat(x', 120, 1)
+ygrid = repeat(y, 1, 120)
+μ, Σ = predict_f(gp2,[vec(xgrid)'; vec(ygrid)'])
+zgrid = reshape(μ, 120, 120)
 
-Ω = 7.3
-x = 0.0:0.002:13π
-y = 3. *sin.(Ω .* collect(x))  
-y′ = 3. * Ω *cos.(Ω .* collect(x)) 
 
-y_reconst = similar(x)
-y_prime = similar(x)
+xmin_phi, xmax_phi =  (minimum(gpphi.x[1,:]), maximum(gpphi.x[1,:]))
+ymin_phi, ymax_phi = (minimum(gpphi.x[2,:]), maximum(gpphi.x[2,:]))
+x_phi = range(xmin_phi, stop=xmax_phi, length=120)
+y_phi = range(ymin_phi, stop=ymax_phi, length=120)
+xgrid_phi = repeat(x_phi', 120, 1)
+ygrid_phi = repeat(y_phi, 1, 120)
+μ_phi, Σ_phi = predict_f(gpphi,[vec(xgrid_phi)'; vec(ygrid_phi)'])
+zgrid_phi = reshape(μ_phi, 120, 120)
 
-k = 5
-base = create_bases(k, Ω)
-base_dot = create_dot_bases(k, Ω)
-b = zeros(2*k + 1)
-b_d = zeros(2*k + 1)
-w = zeros(2*k + 1)
-
-#
-for (idx, t) in enumerate(x)
-    update_basis!(b, base, t)
-    update_basis!(b_d, base_dot, t)
-    update_weights!(w, b, y[idx], 0.005)
-    y_reconst[idx] = dot(w, b)
-    y_prime[idx] = dot(w, b_d)
-end
-
-fig = Figure(resolution=(500, 500))
-ax = Axis(fig[1, 1])
-lines!(ax, x, y, color=:black)
-lines!(ax, x, y′, color=:red)
-lines!(ax, x, y_reconst, color=:black, linestyle=:dash)
-lines!(ax, x, y_prime, color=:red, linestyle=:dash)
+using GLMakie
+GLMakie.activate!()
+z_index = Observable(1.)
+lvls = @lift(range(0., $z_index, step=$z_index))
+fig = Figure(resolution=(1400, 700), fontsize=24, fonts = (; regular = "CMU Serif"))
+#ax = Axis3(fig[1, 1], azimuth = 0., elevation=-π/2) 
+ax = Axis3(fig[1, 1], xlabel=L"R", ylabel=L"\omega", zlabel=L"\Gamma")
+CairoMakie.surface!(ax, xgrid, ygrid, zgrid, colormap=:Spectral_11, shading=false)
+scatterlines!(ax, R_2, 1.75 .* ones(length(R_2)), Γ_2, color=:black)
+scatterlines!(ax, R_3, 2. .* ones(length(R_3)), Γ_3, color=:black)
+scatterlines!(ax, R_4, 2.25 .* ones(length(R_4)), Γ_4, color=:black)
+scatterlines!(ax, R_5, 2.5 .* ones(length(R_5)), Γ_5, color=:black)
+scatterlines!(ax, R_6, 2.75 .* ones(length(R_6)), Γ_6, color=:black)
+scatterlines!(ax, R_7, 3. .* ones(length(R_7)), Γ_7, color=:black)
+scatterlines!(ax, R_8, 3.25 .* ones(length(R_8)), Γ_8, color=:black)
+scatterlines!(ax, R_9, 3.5 .* ones(length(R_9)), Γ_9, color=:black)
+scatterlines!(ax, R_10, 3.75 .* ones(length(R_10)), Γ_10, color=:black)
+scatterlines!(ax, R_11, 4. .* ones(length(R_11)), Γ_11, color=:black)
+CairoMakie.contour3d!(ax, x, y, zgrid'; levels=lvls, color=:white, linewidth=5.)
+ax2 = Axis(fig[1, 2], xlabel=L"\omega", ylabel=L"R")
+GLMakie.contour!(ax2, y, x, zgrid, levels=lvls, color=:black, linewidth=3.)
+sl = Slider(fig[1, 3], horizontal = false, range = 1:0.25:15)
+connect!(z_index, sl.value)
 fig
 
-function perfect_μ(Ω, Δt)
-    return 2*sin(Ω*Δt) * (1 - sin(Ω * Δt))/(cos(Ω*Δt))^2
-end
+
+CairoMakie.activate!(type="svg")
+fig_heat = Figure(resolution=(800, 700), fontsize=28, fonts = (; regular = "CMU Serif"))
+ax2 = Axis(fig_heat[1, 1], xlabel=L"\omega", ylabel=L"R")
+heater = CairoMakie.heatmap!(ax2, ygrid, xgrid, zgrid; levels=3.:1.5:15, colormap=Reverse(:Spectral_11), colorrange=(3, 15.), highclip=:gray) 
+CairoMakie.contour!(ax2, y, x, zgrid; levels=3.:1.5:15, color=:black)
+scatter!(ax2, p_no_bwd.Ω_arr, r_bwd, marker=:ltriangle, color=:red, label="Backward")
+scatter!(ax2, p_no_fwd.Ω_arr, r_fwd, marker=:rtriangle, color=:black, label="Forward")
+scatter!(ax2, p_no_bwd_9.Ω_arr, r_bwd_9, marker=:ltriangle, color=:red)
+scatter!(ax2, p_no_fwd_9.Ω_arr, r_fwd_9, marker=:rtriangle, color=:black)
+scatter!(ax2, p_no_bwd_45.Ω_arr, r_bwd_45, marker=:ltriangle, color=:red)
+scatter!(ax2, p_no_fwd_45.Ω_arr, r_fwd_45, marker=:rtriangle, color=:black)
+CairoMakie.xlims!(ax2, [1.75, 4.])
+CairoMakie.ylims!(ax2, [0.2, 4.1])
+axislegend(position=:lt)
+Colorbar(fig_heat[1, 2][1, 1], heater, ticks=3:1.5:15, vertical=true)
+Label(fig_heat[1, 2][1, 2], L"\Gamma", tellheight=false, rotation=π/2)
+fig_heat
+
+
+GLMakie.activate!()
+fig_trans = Figure(resolution = (800, 800)) 
+ax_trans = Axis3(fig_trans[1, 1])
+surface!(ax_trans, xgrid, zgrid, ygrid, colormap=[:gray, :gray], transparency=true, shading=true)
+ylims!(ax_trans, (0, 32.))
+fig_trans
+
+
+## experiments with otf fourier
+# Ω = 7.3
+# x = 0.0:0.002:13π
+# y = 3. *sin.(Ω .* collect(x))  
+# y′ = 3. * Ω *cos.(Ω .* collect(x)) 
+
+# y_reconst = similar(x)
+# y_prime = similar(x)
+
+# k = 5
+# base = create_bases(k, Ω)
+# base_dot = create_dot_bases(k, Ω)
+# b = zeros(2*k + 1)
+# b_d = zeros(2*k + 1)
+# w = zeros(2*k + 1)
+
+# #
+# for (idx, t) in enumerate(x)
+#     update_basis!(b, base, t)
+#     update_basis!(b_d, base_dot, t)
+#     update_weights!(w, b, y[idx], 0.005)
+#     y_reconst[idx] = dot(w, b)
+#     y_prime[idx] = dot(w, b_d)
+# end
+
+# fig = Figure(resolution=(500, 500))
+# ax = Axis(fig[1, 1])
+# lines!(ax, x, y, color=:black)
+# lines!(ax, x, y′, color=:red)
+# lines!(ax, x, y_reconst, color=:black, linestyle=:dash)
+# lines!(ax, x, y_prime, color=:red, linestyle=:dash)
+# fig
+
+# function perfect_μ(Ω, Δt)
+#     return 2*sin(Ω*Δt) * (1 - sin(Ω * Δt))/(cos(Ω*Δt))^2
+# end
